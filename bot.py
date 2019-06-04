85489e5e5ce6d5542da36c1e9f525a786747066c
Erik <hall.erik@gmail.com>
2019-06-04 13:16:56 -0600

    ready to cron this thing up.

--- bot.py
+++ bot.py
@@ -32,34 +32,36 @@ last_tweet_id = 0
 with open('last_tweet_id') as f:
     last_tweet_id = int(f.readline())
 
-while True:
+print(time.strftime("Starting at %A, %d. %B %Y %I:%M:%S %p"))
+try:
     aaron_timeline = api.user_timeline(
         user_id=14761655, since_id=last_tweet_id)
+except expression as identifier:
+    aaron_timeline = []    
 
-    for result in aaron_timeline:
-        if result.id > last_tweet_id:
-            print('Found a new Tweet')
-            with open('last_tweet_id', 'w') as f:
-                f.write(str(result.id))
-            last_tweet_id = result.id
-            roll = random.randint(1,3)
-            if roll == 1:
-                if result.in_reply_to_status_id == None and \
-                    result.text[:4] != 'RT @':
-                    try:
-                        print('Replying...')
-                        status = random.choice(stati)
-                        api.update_status(
-                            status=status,
-                            in_reply_to_status_id=result.id,
-                            auto_populate_reply_metadata=True
-                        )
-                        print('Done.')
-                    except Exception as e:
-                        print('Something went wrong.')
-                else:
-                    print("It's a reply or RT, ignoring...")
+for result in aaron_timeline:
+    if result.id > last_tweet_id:
+        print('Found a new Tweet:')
+        print(result.text[:15])
+        with open('last_tweet_id', 'w') as f:
+            f.write(str(result.id))
+        last_tweet_id = result.id
+        roll = random.randint(1,3)
+        if roll == 1:
+            if result.in_reply_to_status_id == None and \
+                result.text[:4] != 'RT @':
+                try:
+                    print('Replying...')
+                    status = random.choice(stati)
+                    api.update_status(
+                        status=status,
+                        in_reply_to_status_id=result.id,
+                        auto_populate_reply_metadata=True
+                    )
+                    print('Done.')
+                except Exception as e:
+                    print('Something went wrong.')
             else:
-                print(f'Rolled a {roll}, ignoring...')
-            
-    time.sleep(10)
+                print("It's a reply or RT, ignoring...")
+        else:
+            print(f'Rolled a {roll}, ignoring...')
